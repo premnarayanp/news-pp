6977ce35c7643dbf7fa976f97c222fb498254e9e
premnarayanp <premnarayanpatel223@gmail.com>
2023-12-06 22:30:58 +0530

    home and navbar components

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
+import {Routes, Route} from 'react-router-dom';
 import React from 'react';
+import {  Home } from '../pages/index';
+import  Navbar  from './Navbar';
 import '../styles/app.css'
-class App extends React.Component{
 
-    render(){
+function App(){
+    
+  const Page404=()=>{
+    return <h1>404</h1>
+  };
 
-    return (
-      <div className="App">
-      My app
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Page404/>} />
+      </Routes>
+    </div>
+  );
 
 }
 export default App;
